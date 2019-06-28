6f05adbba2bbaee84fc84fc9fff4be420ecf0e93
Adam Mátl <adam.matl@deso.cz>
2019-06-28 15:11:03 +0200

    podpora markdown

--- src/TemplateFilters.php
+++ src/TemplateFilters.php
@@ -10,8 +10,12 @@ declare(strict_types=1);
 
 namespace Ublaboo\ApiDocu;
 
+use Parsedown;
+
 class TemplateFilters
 {
+	private static $parsedown;
+
 	public static function common(string $filter): ?string
 	{
 		if (method_exists(__CLASS__, $filter)) {
@@ -24,19 +28,7 @@ class TemplateFilters
 
 	public static function description(string $text): string
 	{
-		$text = nl2br($text);
-		$text = str_replace(["\n", "\n\r", "\r\n", "\r"], '', $text);
-
-		$text = preg_replace_callback('/<json><br \/>(.*?)<\/json>/s', function ($item) {
-			$s = '<br><pre class="apiDocu-json">' . str_replace('<br>', '', end($item)) . '</pre>';
-			$s = preg_replace('/(\s)"([^"]+)"/', '$1<span class="apiDocu-string">"$2"</span>', $s);
-			$s = preg_replace('/\/\/(.*?)<br \/>/', '<span class="apiDocu-comment">//$1</span><br>', $s);
-
-			return $s;
-		}, $text);
-
-		$text = preg_replace('/\*\*([^*]*)\*\*/', '<strong>$1</strong>', $text);
-
-		return (string) $text;
+		self::$parsedown = new Parsedown();
+		return self::$parsedown->text($text);
 	}
 }
